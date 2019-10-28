a765601f60bc364f3457f2e10ee7783dec9fd8ce
davytchi <davytchi@davytchis-macbook-pro.local>
2019-10-27 20:13:40 -0700

    unit 2 update

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by davytchi on 10/27/19.
+//  Copyright © 2019 davytchi. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
